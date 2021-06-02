a50dbcbbee8ef37aa27782cde962a8448b3c3b5a
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-06-02 20:26:27 +0200

    Added initial architecture with an entity, repository and a saving functionality.

--- src/main/java/com/example/liquibasedemo/service/Saver.java
+++ src/main/java/com/example/liquibasedemo/service/Saver.java
@@ -1,5 +1,24 @@
 package com.example.liquibasedemo.service;
 
+import com.example.liquibasedemo.entity.Pokemon;
+import com.example.liquibasedemo.repository.Pokedex;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Saver {
+
+    private Pokedex pokedex;
+
+    @Autowired
+    public Saver(Pokedex pokedex) {
+        this.pokedex = pokedex;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initialPokedexInsertion() {
+        pokedex.save(new Pokemon("Cubone"));
+    }
 }
